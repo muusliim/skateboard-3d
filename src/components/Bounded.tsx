21f0ef0b3d56993a5c6d68efd4662ccd337d3132
Muslim <81385968+muslimon@users.noreply.github.com>
2025-03-12 03:04:38 +0300

    type error fix

--- src/components/Bounded.tsx
+++ src/components/Bounded.tsx
@@ -2,7 +2,7 @@ import { CSSProperties, ElementType, ReactNode } from "react";
 import clsx from "clsx";
 
 type BoundedProps = {
-  as?: ElementType;
+  as?: ElementType | string;
   className?: string;
   style?: CSSProperties & { [key: string]: number };
   children: ReactNode;
